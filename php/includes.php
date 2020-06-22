15b4dd5bc5dcd898dc6ad63229524be077fd949b
monoxide13 <git@monoxide13.com>
2020-06-21 22:43:30 -0500

    Changed hit_log shortcut to not through error if unable to write to file.

--- php/includes.php
+++ php/includes.php
@@ -1,10 +1,11 @@
 <?php
-
-if ( ! function_exists('hit_log')) {
-   function hit_log ( $log )  {
+if(!function_exists('hit_log')){
+   function hit_log($log){
       $logFile = fopen('/var/www/rv/rv.log', 'a');
+	  if($logFile==false)
+	  	return;
       if ( is_array( $log ) || is_object( $log ) ) {
-         fwrite($logFile, print_r( $log, true )."\n");
+         fwrite($logFile, print_r( $log, true ));
       } else {
          fwrite($logFile, $log."\n");
       }
