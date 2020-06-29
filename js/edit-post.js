c8c50a24cd38b12c13dc1d87a97438b8f9e8f95d
monoxide13 <git@monoxide13.com>
2020-06-28 22:19:14 -0500

    Corrected issue where adding new item would not allow cost to 3 decimal places.

--- js/edit-post.js
+++ js/edit-post.js
@@ -7,7 +7,7 @@ function purchase_records_add_meta_row(){
 		<td class='pr_c2'><input id='pr_i_id[]' name='pr_i_id[]' type='number' value='0' class='purchase_records_metabox_item_table_items' readonly='true'></td>
 		<td class='pr_c3'><input id='pr_i_nm[]' name='pr_i_nm[]' type='textbox' value='' class='purchase_records_metabox_item_table_items' ></td>
 		<td class='pr_c4'><input id='pr_i_is[]' name='pr_i_is[]' type='checkbox' value='0' class='purchase_records_metabox_item_table_items' style='position:relative;left:25%;'></td>
-		<td class='pr_c5'><input id='pr_i_cs[]' name='pr_i_cs[]' type='number' value='' class='purchase_records_metabox_item_table_items' step='0.01'></td>
+		<td class='pr_c5'><input id='pr_i_cs[]' name='pr_i_cs[]' type='number' value='' class='purchase_records_metabox_item_table_items' step='0.001'></td>
 		<td class='pr_c6'><input id='pr_i_qt[]' name='pr_i_qt[]' type='number' value='1' class='purchase_records_metabox_item_table_items' step='1'></td>
 		<td class='pr_c7'><input id='pr_i_wl[]' name='pr_i_wl[]' type='textbox' value='' class='purchase_records_metabox_item_table_items' ></td></tr>`;
 		$j("#purchase_records_metabox_items_table").append(adding);
